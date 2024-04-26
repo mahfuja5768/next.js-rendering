1a15deaf16b9bc465eff0fc2f40c156bae7a67c9
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2024-04-26 11:33:27 +0600

    add different rendering proccess

---
+++ app/isr/posts/page.js
@@ -0,0 +1,31 @@
+import { getPosts } from "@/apis/apis";
+import Link from "next/link";
+
+export default async function Posts() {
+  //isr
+
+  //   const posts = await getPosts("revalidated");
+
+  //ssg
+  const posts = await getPosts();
+
+  return (
+    <div>
+      <h1>All posts</h1>
+      <div className="mt-4">
+        <ul>
+          {posts.map((post) => (
+            <Link
+              prefetch={false}
+              className="block hover:border-0"
+              href={`/isr/posts/${post.id}`}
+              key={post.id}
+            >
+              {post.id}. {post.title}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
